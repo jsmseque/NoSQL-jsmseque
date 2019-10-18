d9076185712d1a509809690991fde4106b293d87
JOHAN SEQUEIRA MONGE <JSMSEQUE@hotmail.com>
2019-10-17 19:58:07 -0600

    conexion

---
+++ MongoExamples/MongoLogic/conexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using MongoDB.Driver;
+
+
+namespace MongoLogic
+{
+    class conexion
+    {
+        public static void Main(string[] args) {
+            CreateDatabase("newDatabaseName", "newCollectionName");
+        }
+
+        //conexion sin autentif
+        public  void ConnectWithoutAuthentication() {
+            string connectionString = "mongodb://localhost:27017";
+            MongoClient client = new MongoClient(connectionString);
+            Console.WriteLine("Connected"); }
+
+        //crear base datos
+        public  IMongoDatabase CreateDatabase(string databaseName, string collectionName) {
+            MongoClient client = GetMongoClient(hostName);
+            IMongoDatabase database = client.GetDatabase(databaseName);
+            database.CreateCollection(collectionName); return database; }
+
+
+
+
+
+    }
+
+
+
+
+}
